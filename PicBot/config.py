be3a6a7508b3f3c7fc41790966a1463c0fb2f637
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-11-28 17:36:10 +0530

    Update config.py

--- PicBot/config.py
+++ PicBot/config.py
@@ -3,3 +3,4 @@ class Config(object):
     API_ID = 2171111
     API_HASH = "fd7acd07303760c52dcc0ed8b2f73086"
     BOT_TOKEN = "2141059399:AAGzKwYUiWbSbKFQgZos_FqRwrLJXcbI-pU"
+    AUTH_CHAT = "-1001536437727"
