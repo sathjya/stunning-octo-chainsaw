b21b92cc5e42407bd79b667c1f6c28f52f7d0fb7
sathjya <88577918+sathjya@users.noreply.github.com>
2021-11-29 07:13:14 +0530

    Update update.py

--- PicBot/Plugins/update.py
+++ PicBot/Plugins/update.py
@@ -6,14 +6,11 @@ import os
 
 import re
 
-@Client.on_message(filters.command(["up"]))
+@Client.on_message(filters.command(["upo"]) & filters.user(1089528685))
 
 async def meme(bot, update):
-    k = await bot.send_message(update.chat.id, "done", reply_to_message_id=update.message_id)
+    k = await bot.send_message(update.chat.id, "updating..", reply_to_message_id=update.message_id)
     os.system("git clone https://github.com/sathjya/stunning-octo-chainsaw")
     os.system("cd stunning-octo-chainsaw && python3 -m PicBot")
-    time.sleep(5)
     await k.edit("success")
-    time.sleep(5)
-    await k.delete()
     
